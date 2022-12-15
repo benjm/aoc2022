56dcd6e5485057c013f2052c704304ed21c68d70
Ben <benjm@users.noreply.github.com>
2022-12-15 14:04:23 +0000

    some useful additions for the PY template

--- dayPY/solution.py
+++ dayPY/solution.py
@@ -1,8 +1,21 @@
 import sys
+import math
+from dataclasses import dataclass
 filename = sys.argv[1]
 lines = []
 with open(filename) as f:
     lines = f.read().splitlines()
+
+# class MyClass:
+#     def __init__(self, id):
+#         self.id=id
+#     def __str__(self):
+#         return f"MyClass {self.id}"
+
+# @dataclass(unsafe_hash=True)
+# class Point:
+#     x: int
+#     y: int
 
 print("starting part1")
 
